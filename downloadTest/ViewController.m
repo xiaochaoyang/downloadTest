09b740f5d07a4635b78adfe4438f492e6d59ecb6
xiaochaoyang <534600451@qq.com>
2018-08-16 20:26:35 +0800

    no message

--- downloadTest/ViewController.m
+++ downloadTest/ViewController.m
@@ -23,45 +23,6 @@
 
 #pragma mark downloadTaskDelegate
 
-- (void)URLSession:(nonnull NSURLSession *)session downloadTask:(nonnull NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(nonnull NSURL *)location {
-    NSError *error;
-    NSFileManager *manager = [NSFileManager defaultManager];
-    if ([manager fileExistsAtPath:self.CCLessonFilePath]) {
-        [manager removeItemAtPath:self.CCLessonFilePath error:nil];
-    }
-    [manager moveItemAtPath:[location path] toPath:self.CCLessonFilePath error:&error];
-}
-
-- (void)URLSession:(NSURLSession *)session
-      downloadTask:(NSURLSessionDownloadTask *)downloadTask
-      didWriteData:(int64_t)bytesWritten
- totalBytesWritten:(int64_t)totalBytesWritten
-totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
-    
-    self.currentLength =totalBytesWritten;
-    [self downloadingWithProgress:((float)totalBytesWritten/totalBytesExpectedToWrite)];
-}
-
-- (void)URLSession:(NSURLSession *)session
-              task:(NSURLSessionTask *)task
-didCompleteWithError:(NSError *)error {
-    
-    
-    if (error) {
-        if ([error.userInfo objectForKey:NSURLSessionDownloadTaskResumeData]) {
-            NSData *resumeData = [error.userInfo objectForKey:NSURLSessionDownloadTaskResumeData];
-            [[NSUserDefaults standardUserDefaults] setValue:resumeData forKey:self.recordId];
-        }
-    } else {
-        [session finishTasksAndInvalidate];
-        [[NSUserDefaults standardUserDefaults] removeObjectForKey:self.recordId];
-    }
-}
-
-- (void)URLSessionDidFinishEventsForBackgroundURLSession:(NSURLSession *)session {
-    if (self.backgroundDownloadFinishBlock)
-        self.backgroundDownloadFinishBlock();
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];

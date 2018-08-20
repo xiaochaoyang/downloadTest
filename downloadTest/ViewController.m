e8161983fa6f2210d7f26a75ad81eebe32eeabc3
xiaochaoyang <534600451@qq.com>
2018-08-20 16:04:34 +0800

    1

--- downloadTest/ViewController.m
+++ downloadTest/ViewController.m
@@ -16,6 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSNumber *num = @1;
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)startDownload:(id)sender {
